26476e7eb86760c97427d35f2d1bf19586fedd45
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-01-03 07:57:42 -0800

    Added more users.api.test, set up npm test script to take bigtodo-test as the database instead of bigtodo so we can drop and populate DB from scratch for test

--- test/users.api.test.js
+++ test/users.api.test.js
@@ -3,15 +3,45 @@ const chaiHttp = require('chai-http');
 const { expect } = chai;
 chai.use(chaiHttp);
 
-// const connection = require('../lib/setup-mongoose');
-// const db = require('./db');
-// const app = require('../lib/app');
+const connection = require('../lib/setup-mongoose');
+const db = require('./db');
+const app = require('../lib/app');
 
 describe ('Users API', () => {
 
   it ('fails test because no tests are written', (done) => {
     expect(true).to.not.be.ok;
     done();
+  });
+
+  before(db.drop(connection));
+
+  const request = chai.request(app);
+
+  const testUser = {
+    username: 'testuser',
+    password: 'testpass',
+    email: 'user@test.com'
+  };
+
+  let authHeader = null;    
+  before(() => {
+    request
+      .post('/api/auth/signup')
+      .send(testUser)
+      .then(({ body }) => {
+        expect(body.token).to.be.ok;
+        authHeader = { Authorization: `Bearer ${body.token}` };
+      });
+  });
+
+  let userId = null;
+
+  it('GET /api/users/:id', () => {
+    request
+      .get(`/api/users/${userId}`)
+      .set(authHeader)
+      .then(res => expect(res.body).to.deep.equal([]));
   });
 
 });
