c23e58449340eaeecf934fd1a31cc253487b3a84
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-01-28 17:36:57 -0800

    Improved user management with /me routes, added some users integration testing

--- test/api-tests/users.api.test.js
+++ test/api-tests/users.api.test.js
@@ -11,39 +11,42 @@ const app = require('../../lib/app');
 
 describe ('Users API', () => {
 
-  it.skip ('fails test because no tests are written', (done) => {
-    expect(true).to.not.be.ok;
-    done();
-  });
-
   before(db.drop(connection));
 
   const request = chai.request(app);
 
   const testUser = {
-    username: 'testuser',
-    password: 'testpass',
-    email: 'user@test.com'
+    username: 'tester',
+    password: 'testerpass',
+    email: 'tester@testing.com'
   };
 
-  let authHeader = null;    
-  before(() => {
+  let authHeader = null;
+
+  before((done) => {
     request
       .post('/api/auth/signup')
       .send(testUser)
       .then(({ body }) => {
         expect(body.token).to.be.ok;
         authHeader = { Authorization: `Bearer ${body.token}` };
-      });
+        done();
+      })
+      .catch(done);
   });
 
-  let userId = null;
+  // let userId = null;
 
-  it.skip ('GET /api/users/:id', () => {
+  it ('GET /api/users/me', (done) => {
     request
-      .get(`/api/users/${userId}`)
+      .get('/api/users/me')
       .set(authHeader)
-      .then(res => expect(res.body).to.deep.equal([]));
+      .then(({ body }) => {
+        expect(body.username).to.equal(testUser.username);
+        expect(body.email).to.equal(testUser.email);
+        done();
+      })
+      .catch(done);
   });
 
   // after((done) => {
